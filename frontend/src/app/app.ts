5f5985589d47922f5b1b2cce04905016a8cf0055
Diego Caceres <diegodanielcaceres10@gmail.com>
2026-02-16 20:31:30 +0100

    Created home page and sections

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,17 @@
 import { Component, signal } from '@angular/core';
+import { Card } from './shared/card/card';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
+import { About } from './sections/about/about';
+import { Experience } from './sections/experience/experience';
+import { Skills } from './sections/skills/skills';
+import { Projects } from './sections/projects/projects';
+import { Testimonials } from './sections/testimonials/testimonials';
+import { Contact } from './sections/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, Footer, Card, About, Experience, Skills, Projects, Testimonials, Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
