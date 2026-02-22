8daf161d3e14287c82b42353c565d40626b704cd
Diego Caceres <diegodanielcaceres10@gmail.com>
2026-02-22 15:31:14 +0100

    Migrate te components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal, HostListener } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AboutComponent } from './sections/about-component/about-component';
+import { ExperiencesComponent } from './sections/experiences-component/experiences-component';
+import { ProjectsComponent } from './sections/projects-component/projects-component';
 
 @Component({
   selector: 'app-root',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, AboutComponent, ExperiencesComponent, ProjectsComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
 })
 export class App {
   isSticky = signal(false);
