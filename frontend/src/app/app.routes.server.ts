03ff54deb4b5e5a4f04220ac2e02af54bf2de398
Diego Caceres <diegodanielcaceres10@gmail.com>
2026-03-01 19:29:11 +0100

    fix(ssr): avoid prerender redirect page on root route

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,7 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  { path: '', renderMode: RenderMode.Prerender },
+  { path: '', renderMode: RenderMode.Client },
   {
     path: ':lang',
     renderMode: RenderMode.Prerender,
