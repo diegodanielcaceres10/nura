ae7672f374e6dee82170e244b5c9f2fb228fb9e1
Diego Caceres <diegodanielcaceres10@gmail.com>
2026-04-29 00:44:34 +0200

    feat: experiences page update

--- frontend/src/app/interfaces/experience.ts
+++ frontend/src/app/interfaces/experience.ts
@@ -1,7 +1,9 @@
 export interface Experience {
   company: string;
   roleKey: string;
-  periodKey: string;
+  roleShortKey: string;
+  startPeriodKey: string;
+  endPeriodKey: string;
   skills: string[];
   responsibilityKeys: string[];
 }
