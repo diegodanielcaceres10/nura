d6af2942eb1f1822d5211e9b36eccdf3884e670e
Diego Caceres <diegodanielcaceres10@gmail.com>
2026-07-28 21:53:35 +0200

    vitest config centralize

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    reporters: ['default', 'junit'],
-    outputFile: {
-      junit: './vitest/junit-report.xml',
-    },
     coverage: {
       provider: 'v8',
       reporter: ['html', 'lcov', 'cobertura', 'text-summary', 'json-summary'],
