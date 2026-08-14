b7fb95e1fcc4dca4964c8c95d861d4ed175d73bf
Diego Caceres <diegodanielcaceres10@gmail.com>
2026-08-14 18:01:10 +0200

    remove signal import unused

--- frontend/src/app/language-selector/language-selector.page.ts
+++ frontend/src/app/language-selector/language-selector.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LocaleService } from '../services/locale/locale.service';
 
 export interface LanguageOption {
--- frontend/src/app/sections/case-study-component/case-study.component.ts
+++ frontend/src/app/sections/case-study-component/case-study.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateKeyPipe } from '../../services/translate/translate-key.pipe';
 import { TitleComponent } from '../../components/title/title.component';
 
